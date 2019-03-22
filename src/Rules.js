17c6b397fd37933768d38ac4581287608a1139ef
Sandy <sangzicao@gmail.com>
2019-03-22 14:44:36 -0700

    add missing rules full house and smallstraight in rules.js

--- src/Rules.js
+++ src/Rules.js
@@ -60,13 +60,21 @@ class SumDistro extends Rule {
 /** Check if full house (3-of-kind and 2-of-kind) */
 
 class FullHouse {
-  // TODO
+  evalRoll = (dice) => {
+    // should match 3 of one die and 2 of another die 
+    return (this.freq(dice).includes(2) && this.freq(dice).includes(3)) ? this.score : 0;
+  }
 }
 
 /** Check for small straights. */
 
 class SmallStraight {
-  // TODO
+  evalRoll = (dice) => {
+    const d = new Set(dice)
+    return ((d.has(2) && d.has(3) && d.has(4) && d.has(5)) ||
+            (d.has(1) && d.has(2) && d.has(3) && d.has(4)) ||
+            (d.has(3) && d.has(4) && d.has(5) && d.has(6))) ? this.score : 0;
+  }
 }
 
 /** Check for large straights. */
@@ -102,10 +110,10 @@ const threeOfKind = new SumDistro({ count: 3 });
 const fourOfKind = new SumDistro({ count: 4 });
 
 // full house scores as flat 25
-const fullHouse = "TODO";
+const fullHouse = new FullHouse({ count: 25});
 
 // small/large straights score as 30/40
-const smallStraight = "TODO";
+const smallStraight = new SmallStraight({ score: 30 });
 const largeStraight = new LargeStraight({ score: 40 });
 
 // yahtzee scores as 50
